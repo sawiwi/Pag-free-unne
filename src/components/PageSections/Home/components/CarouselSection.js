545963420a7fc9dc98e9bc6743d00f224999652a
fabian <fabian.salas.astete@gmail.com>
2023-11-27 15:54:57 -0300

    feat: add button wsp contact and design about me in carousel

--- src/components/PageSections/Home/components/CarouselSection.js
+++ src/components/PageSections/Home/components/CarouselSection.js
@@ -2,15 +2,10 @@ import React from 'react';
 import MainCarousel from '../../../Carousel/MainCarousel';
 import { mainCarouselData } from '../../../../data';
 
-import AboutMinified from '../../../../pages/AboutMinified';
-
 const CarouselSection = () => (
   <div className="grid grid-cols-1 xl:grid-cols-3" id='IndexSection'>
-      <div className="grid col-span-1 xl:col-span-1">
-        <AboutMinified/>
-      </div>
-      <div className="grid col-span-1 xl:col-span-2 ">
-      <div className="object-cover w-[100%] h-[400px] xl:h-[600px]" >
+      <div className="grid col-span-1 xl:col-span-3 ">
+      <div className="object-cover w-[100%] h-[400px] xl:h-[500px]" >
         <MainCarousel data={mainCarouselData} />
       </div>
       </div>
--- src/data/index.js
+++ src/data/index.js
@@ -1,29 +1,35 @@
 import Slide1 from '../assets/img/carousel/slide1.jpg';
 import Slide4 from '../assets/img/carousel/slide4.jpg';
 
+import AboutMeImg from '../assets/img/me/me_Photo.jpg'
+
 /** Main Carousel Data */
 export const mainCarouselData = [
   {
     id: 1,
-    // title: 'Asegura tu inversión con Remate seguro',
+    title: 'Fabian Salas Astete.',
+    imgMe: AboutMeImg,
     // desc: 'Garantizamos hasta un 15% de retorno por tu inversión',
     img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689974575/Projects/unne/img/hero/remate3.jpg',
   },
   {
     id: 2,
-    // title: '¡Encuentra tu financiamiento ideal!',
+    title: 'Fabian Salas Astete.',
+    imgMe: AboutMeImg,
     // desc: 'En Alianza con los Bancos más importantes del país',
     img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689975271/Projects/unne/img/hero/financiamiento2.jpg',
   },
   {
     id: 3,
-    // title: '¡Simplifica tus trámites legales con nuestros abogados inmobiliarios!',
+    title: 'Fabian Salas Astete.',
+    imgMe: AboutMeImg,
     // desc: 'Nos encargamos de todo el proceso de la postventa de tu propiedad',
     img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689975597/Projects/unne/img/hero/tramite2.jpg',
   },
   {
     id: 4,
-    // title: '¡Deja la venta de tu propiedad en mano de expertos!',
+    title: 'Fabian Salas Astete.',
+    imgMe: AboutMeImg,
     // desc: 'Somos la corredora más grande del país, con más de 6000 publicaciones activas',
     img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689974937/Projects/unne/img/hero/venta.jpg',
   },
