a92b457d224aec4fe2bdcfa9b6d929132c659622
fabian <fabian.salas.astete@gmail.com>
2023-12-04 12:31:20 -0300

    feat: add Link Component carousel section

--- src/components/Carousel/MainCarousel.js
+++ src/components/Carousel/MainCarousel.js
@@ -3,6 +3,8 @@ import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import '../../assets/styles/components/Carousel/MainCarousel.css';
 import { Fade } from 'react-awesome-reveal';
+import { Link } from 'react-scroll';
+
 
 const MainCarousel = ({ data }) => {
   return (
@@ -34,7 +36,10 @@ const MainCarousel = ({ data }) => {
                         {slide.title}
                       </h2>
                       <div className='mx-4 xl:mx-56'>
-                        <a href='#AboutSectionScroll'  type='button' className='inline-flex items-center drop-shadow-lg p-10 py-2 w-52 m-2 text-center  xl:w-52 xl:h-12 text-white bg-primary rounded-lg hover:bg-primary-opacity focus:ring-4 focus:outline-none focus:ring-primary-300 text-lg'>Más sobre mí</a>
+                        <Link to='AboutSectionScroll' smooth="true" duration={700} offset={-150}  className='inline-flex items-center drop-shadow-lg p-10 py-2 w-52 m-2 text-center  xl:w-52 xl:h-12 text-white bg-primary rounded-lg hover:bg-primary-opacity focus:ring-4 focus:outline-none focus:ring-primary-300 text-lg cursor-pointer'>
+                          <span className='text-lg font-medium capitalize mx-auto max-h-10'>Más sobre mí</span>
+                        </Link>
+                        
                       </div>  
                     </div>
                   </Fade>
